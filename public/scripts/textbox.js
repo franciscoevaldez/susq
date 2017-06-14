73df3e60f5b2be64aaf160c617d981039ca0f16d
franciscoevaldez <franciscoevaldez@gmail.com>
2017-06-14 18:05:59 -0300

    Improved validation
    Refactored

--- public/scripts/textbox.js
+++ public/scripts/textbox.js
@@ -1,45 +1,66 @@
-function susTextBox (_inputID) {
+function susTextBox (_inputID, _customValidation) {
   var _elem = document.getElementById(_inputID);
   var _err = document.getElementById(_inputID + '--error')
 
-  var _errorClass = 'input--error',
-      validStates = ['This field is required', 'OK'];
+  var _errorClass = 'input--error';
+      //validStates = ['This field is required', 'OK'];
 
   // validation
   function _validate(){
-    return (_elem.value) == "" ? 0 : 1
+
+    var state = {};
+
+    if (_customValidation) {
+      // custom validation: run function (passing value as argument)
+      state = _customValidation(_elem.value);
+    } else {
+      // common validation: return false if empty
+      if (_elem.value == "") {
+        state = {valid: false, message: false}
+      } else {
+        state = {valid: true, message: false}
+      }
+    }
+
+    // catch standard empty and success messages
+    state.message = (state.message == false && state.valid == true) ? "OK" : state.message;
+    state.message = (state.message == false && state.valid == false) ? "This field is required" : state.message;
+
+    _updateUIforState(state);
+
+    return state;
+
   }
 
-  function getValidState(){
-    return validStates[_validate()];
-  }
+  function _updateUIforState(_state){
 
-  // on blur update
-  function _wasBlurred(){
-    // validate
-    var state = _validate();
     // reset styles
     _elem.classList.remove(_errorClass);
     _err.innerHTML = ""
     // if validation fails, apply styles
-    if (state != 1) {
+    if (_state.valid == false) {
       _elem.classList.add(_errorClass);
-      _err.innerHTML = validStates[state];
+      _err.innerHTML = _state.message;
     }
 
+  }
+
+  function getValidState(){
+    var state = _validate();
+    return state.valid;
+  }
+
+  // on blur update
+  function _wasBlurred(){
+    var state = _validate();   // validate
   };
 
   // listeners
   _elem.addEventListener("blur", _wasBlurred, true);
 
   return{
-    validate : getValidState
+    isValid : getValidState,
+    value : _elem.value
   }
 
 }
-
-susTextBox.prototype = function(){
-
-
-
-};
--- public/scripts/ui.js
+++ public/scripts/ui.js
@@ -17,13 +17,10 @@ var ui = (function(){
   btnStart.addEventListener("click", function(event){
 
     event.preventDefault();
-    var setupValidation = validateSetup.all();
-    if (!setupValidation.isValid) {
-      setupValidation.title()
-      setupValidation.mail()
-      return
-    }
+    var isTitleValid = ui.title.isValid(),
+        isEmailValid = ui.email.isValid();
 
+    if ((!isTitleValid) || (!isEmailValid)) { return }
 
     app.changeToStep(1);
 
@@ -53,107 +50,23 @@ var ui = (function(){
   })
 
   // inputs --------------------------------------------------------------------
-  var titleInput = new susTextBox('input--title');
-  var emailInput = new susTextBox('input--email');
-
-  /*
-
-  // UI title input (step 1) ---------------------------------------------------
-  var uiTitleInput = (function(){
-
-    // elements
-    var input = document.getElementById('input--title'),
-    errorSpan = document.getElementById('input--title--error');
-    // properties
-    var errorClass = "input--error",
-    validStates = ['This field is required', 'OK'];
-
-    // validation
-    function _validate(){
-      return (input.value) == "" ? 0 : 1
+  var titleInput = new susTextBox('input--title', false);
+  var emailInput = new susTextBox('input--email', function(_value){
+    if (_value == "") {
+      return {valid: false, message: false}     // email validation 1: should not be empty
+    } else if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(_value))) {
+      return {valid: false, message: "Please enter a valid email"}  // email validation 2: should match the email pattern
+    } else {
+      return {valid: true, message: false} // otherwise is valid
     }
+  });
+  var nameInput = new susTextBox('response--name', false);
 
-    function getValidState(){
-      return validStates[_validate()];
-    }
-
-    // on blur update
-    function _wasBlurred(){
-      // validate
-      var state = _validate();
-      // reset styles
-      input.classList.remove(errorClass);
-      errorSpan.innerHTML = ""
-      // if validation fails, apply styles
-      if (state != 1) {
-        input.classList.add(errorClass);
-        errorSpan.innerHTML = validStates[state];
-      }
-
-    }
-
-    // listeners
-    input.addEventListener("blur", _wasBlurred, true);
-
-    return{
-      validate : getValidState
-    }
-
-  })();
-  */
-  /*
-  var uiEmailInput = (function(){
-
-    // elements
-    var input = document.getElementById('input--email'),
-    errorSpan = document.getElementById('input--email--error');
-    // properties
-    var errorClass = "input--error",
-    validStates = ['This field is required', 'OK', 'Please enter a valid email address'];
-
-    // validation
-    function _validate(){
-      var value = input.value
-      // mail should not be empty
-      if(value == ''){ return 0 }
-      // mail should follow this pattern
-      if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value))){ return 2 }
-      // if so, it is valid
-      return 1
-    }
-
-    function getValidState(){
-      return validStates[_validate()];
-    }
-
-    // on blur update
-    function _wasBlurred(){
-      // validate
-      var state = _validate();
-      // reset styles
-      input.classList.remove(errorClass);
-      errorSpan.innerHTML = ""
-      // if validation fails, apply styles
-      if (state != 1) {
-        input.classList.add(errorClass);
-        errorSpan.innerHTML = validStates[state];
-      }
-
-    }
-
-    // listeners
-    input.addEventListener("blur", _wasBlurred, true);
-
-    return{
-      validate : getValidState
-    }
-
-  })();
-  */
 
   return {
     title : titleInput,
-    email : emailInput
+    email : emailInput,
+    name  : nameInput
   }
 
 })();
