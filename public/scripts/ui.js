de006bd36c2a94b5cfb2cd8b4ccefa028d2a6a63
franciscoevaldez <franciscoevaldez@gmail.com>
2017-06-19 11:21:06 -0300

    Final screen

--- public/scripts/ui.js
+++ public/scripts/ui.js
@@ -11,10 +11,11 @@ var ui = (function(){
       btnRespond  = document.getElementById('btn--respond'),
       btnSend     = document.getElementById('btn--send');
 
-  // Start button (step 0)
-  /* Should validate step 0 inputs, store
-     the new questionnaire and advance to next step */
+  // Start button
   btnStart.addEventListener("click", function(event){
+
+    /* Should validate step 0 inputs, store
+     the new questionnaire and advance to next step */
 
     event.preventDefault();
     var isTitleValid = ui.title.isValid(),
@@ -26,9 +27,9 @@ var ui = (function(){
 
   }, true);
 
-  // Respond button (step 1)
-  /* Should validate name and scroll to first question */
+  // Respond button
   btnRespond.addEventListener('click', function(event){
+    /* Should validate name and scroll to first question */
     event.preventDefault();
 
     if( !(ui.name.isValid()) ){
@@ -39,10 +40,10 @@ var ui = (function(){
     scrollManager.scrollTo(0);
   });
 
-  // Send response button (step 1)
-  /* Should validate user name, all answers, request score,
-     send the response to server and move to the next step */
+  // Send response button
   btnSend.addEventListener("click", function(event){
+    /* Should validate user name, all answers, request score,
+     send the response to server and move to the next step */
 
     event.preventDefault();
     // validation
@@ -67,12 +68,14 @@ var ui = (function(){
 
     // advance to thank you
     console.log(answer);
-    window.alert("sent!");
+    app.changeToStep(2);
 
   })
 
-  // inputs --------------------------------------------------------------------
+  // Title input
   var titleInput = new susTextBox('input--title', false);
+
+  // Email input
   var emailInput = new susTextBox('input--email', function(_value){
     if (_value == "") {
       return {valid: false, message: false}     // email validation 1: should not be empty
@@ -82,6 +85,8 @@ var ui = (function(){
       return {valid: true, message: false} // otherwise is valid
     }
   });
+
+  // Name input
   var nameInput = new susTextBox('response--name', false);
 
 
